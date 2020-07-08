8b5be91e28ae612a50ecb2183af4f74d226138e8
Júlio Nery <julio_cesar.an@hotmail.com>
2020-07-08 18:49:22 -0300

    :tada: finalizando o desafio

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,20 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const totalCart = products.reduce((total, product) => {
+      const result = product.price * product.quantity;
+      return total + result;
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(totalCart);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
-
-    return 0;
+    const qtdTotalCart = products.reduce((total, product) => {
+      const result = product.quantity;
+      return total + result;
+    }, 0);
+    return qtdTotalCart;
   }, [products]);
 
   return (
